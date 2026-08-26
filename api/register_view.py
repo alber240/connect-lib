2da7280befeec84725dec6acedceabc76225bf4e
alber240 <bigchicha74@gmail.com>
2026-08-26 06:04:31 +0200

    Fix: Remove null bytes and binary files, add registration endpoint

--- api/register_view.py
+++ api/register_view.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
+﻿from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
