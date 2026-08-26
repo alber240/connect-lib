0158e1cec660a86e90951ae8b442321a39837cb9
alber240 <bigchicha74@gmail.com>
2026-08-26 12:25:36 +0200

    Fix: Add missing CountySerializer and all required serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,58 @@
 ﻿from rest_framework import serializers
-from .models import County, Institution, Suggestion, News, Comment, Rating
+from .models import County, Institution, Suggestion, News, Comment, Rating, Media
 
-# ... your existing serializers ...
+class CountySerializer(serializers.ModelSerializer):
+    institution_count = serializers.IntegerField(source='institutions.count', read_only=True)
+    
+    class Meta:
+        model = County
+        fields = ['id', 'name', 'description', 'capital', 'population', 'institution_count']
+
+class InstitutionSerializer(serializers.ModelSerializer):
+    county_name = serializers.CharField(source='county.name', read_only=True)
+    
+    class Meta:
+        model = Institution
+        fields = [
+            'id', 'name', 'category', 'county', 'county_name',
+            'district', 'address', 'phone', 'phone2', 'email', 'website',
+            'description', 'services', 'opening_hours',
+            'latitude', 'longitude', 'google_maps_link',
+            'is_verified', 'is_featured', 'source', 'cover_image',
+            'video_url', 'likes', 'average_rating', 'total_ratings',
+            'created_at', 'updated_at'
+        ]
+
+class InstitutionDetailSerializer(serializers.ModelSerializer):
+    county_name = serializers.CharField(source='county.name', read_only=True)
+    media_files = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Institution
+        fields = '__all__'
+    
+    def get_media_files(self, obj):
+        return MediaSerializer(obj.media_files.all(), many=True).data
+
+class SuggestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Suggestion
+        fields = ['id', 'institution_name', 'suggestion_type', 'current_info', 
+                  'new_info', 'suggester_name', 'suggester_phone', 'suggester_email',
+                  'status', 'admin_notes', 'created_at', 'updated_at']
+
+class NewsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = News
+        fields = ['id', 'title', 'content', 'category', 'county', 'institution', 
+                  'source', 'featured_image', 'video_url', 'is_published', 
+                  'created_at', 'updated_at']
+
+class MediaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Media
+        fields = ['id', 'institution', 'media_type', 'file', 'video_url', 
+                  'title', 'caption', 'is_cover', 'order', 'uploaded_at']
 
 class CommentSerializer(serializers.ModelSerializer):
     user_username = serializers.CharField(source='user.username', read_only=True)
